6c6baed69b314a6a908e126f3914765e77d89224
Tasviq Hossain <t9hossai@uwaterloo.ca>
2023-10-06 00:48:21 -0400

    Add initializations

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+const globeConfig = {
+    continuousRotation: true, // flag to track if globe should rotate continuously
+    initialRotationSpeed: 200,
+    isDragging: false, // When mouse used to rotate the globe
+};
+
+const dimensions = {
+    width: 960,
+    height: 600,
+};
+
+const svg = d3.select('svg').attr('width', dimensions.width).attr('height', dimensions.height);
+const projection = d3.geoOrthographic();
+const path = d3.geoPath().projection(projection);
+const origin = [dimensions.width / 2, dimensions.height / 2];
+
+// Get input from "Visit a Country" dropdown
+const countryInput = document.getElementById('country');
+countryInput.addEventListener('change', updateSelectedCountry)
+
+// save the coordinates of the last selected country, 
+// will be the inital viewpoint for the d3.interpolate in the visitCountry() function
+let [gCurrentLat, gCurrentLng] = projection.invert(origin);
+
+
 /** 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
